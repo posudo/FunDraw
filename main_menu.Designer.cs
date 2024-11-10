24169172c572ea055eb870526ec198249ca8d6b6
posudo <mmttriet@gmail.com>
2024-11-10 20:07:53 +0700

    chore: add main menu form

---
+++ main_menu.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace FunDraw
+{
+    partial class main_menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            shPlayer = new Guna.UI2.WinForms.Guna2Shapes();
+            btThamGia = new Guna.UI2.WinForms.Guna2Button();
+            btHoSo = new Guna.UI2.WinForms.Guna2Button();
+            tbMaPhong = new Guna.UI2.WinForms.Guna2TextBox();
+            btDangXuat = new Guna.UI2.WinForms.Guna2Button();
+            btTaoPhong = new Guna.UI2.WinForms.Guna2Button();
+            cpbLogo = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            cpbAvatar = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            panel_main = new Panel();
+            lbPlayer = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lbDangChoi = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            lbThamGia = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)cpbLogo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)cpbAvatar).BeginInit();
+            panel_main.SuspendLayout();
+            SuspendLayout();
+            // 
+            // shPlayer
+            // 
+            shPlayer.Anchor = AnchorStyles.None;
+            shPlayer.BackColor = Color.Transparent;
+            shPlayer.BorderColor = Color.Transparent;
+            shPlayer.BorderThickness = 0;
+            shPlayer.FillColor = Color.FromArgb(224, 224, 224);
+            shPlayer.Location = new Point(5, 129);
+            shPlayer.Margin = new Padding(4, 3, 4, 3);
+            shPlayer.Name = "shPlayer";
+            shPlayer.PolygonSkip = 1;
+            shPlayer.Rotate = 0F;
+            shPlayer.RoundedEdges = customizableEdges1;
+            shPlayer.Shape = Guna.UI2.WinForms.Enums.ShapeType.Rounded;
+            shPlayer.Size = new Size(233, 378);
+            shPlayer.TabIndex = 10;
+            shPlayer.Text = "guna2Shapes1";
+            shPlayer.UseTransparentBackground = true;
+            shPlayer.Zoom = 80;
+            // 
+            // btThamGia
+            // 
+            btThamGia.Anchor = AnchorStyles.None;
+            btThamGia.Animated = true;
+            btThamGia.BorderRadius = 12;
+            btThamGia.CustomizableEdges = customizableEdges2;
+            btThamGia.DisabledState.BorderColor = Color.DarkGray;
+            btThamGia.DisabledState.CustomBorderColor = Color.DarkGray;
+            btThamGia.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btThamGia.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btThamGia.FillColor = Color.FromArgb(160, 210, 235);
+            btThamGia.Font = new Font("Segoe UI", 12F);
+            btThamGia.ForeColor = Color.Black;
+            btThamGia.Location = new Point(378, 411);
+            btThamGia.Margin = new Padding(4, 3, 4, 3);
+            btThamGia.Name = "btThamGia";
+            btThamGia.ShadowDecoration.CustomizableEdges = customizableEdges3;
+            btThamGia.Size = new Size(174, 42);
+            btThamGia.TabIndex = 1;
+            btThamGia.Text = "Tham gia";
+            // 
+            // btHoSo
+            // 
+            btHoSo.Anchor = AnchorStyles.None;
+            btHoSo.Animated = true;
+            btHoSo.BackColor = Color.FromArgb(224, 224, 224);
+            btHoSo.BorderRadius = 12;
+            btHoSo.CustomizableEdges = customizableEdges4;
+            btHoSo.DisabledState.BorderColor = Color.DarkGray;
+            btHoSo.DisabledState.CustomBorderColor = Color.DarkGray;
+            btHoSo.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btHoSo.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btHoSo.FillColor = Color.FromArgb(160, 210, 235);
+            btHoSo.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btHoSo.ForeColor = Color.Black;
+            btHoSo.Location = new Point(50, 362);
+            btHoSo.Margin = new Padding(4, 3, 4, 3);
+            btHoSo.Name = "btHoSo";
+            btHoSo.ShadowDecoration.CustomizableEdges = customizableEdges5;
+            btHoSo.Size = new Size(144, 39);
+            btHoSo.TabIndex = 2;
+            btHoSo.Text = "Hồ sơ";
+            // 
+            // tbMaPhong
+            // 
+            tbMaPhong.Anchor = AnchorStyles.None;
+            tbMaPhong.BorderRadius = 12;
+            tbMaPhong.Cursor = Cursors.IBeam;
+            tbMaPhong.CustomizableEdges = customizableEdges6;
+            tbMaPhong.DefaultText = "Mã phòng";
+            tbMaPhong.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            tbMaPhong.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            tbMaPhong.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            tbMaPhong.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            tbMaPhong.FillColor = Color.FromArgb(224, 224, 224);
+            tbMaPhong.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbMaPhong.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbMaPhong.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbMaPhong.Location = new Point(378, 362);
+            tbMaPhong.Margin = new Padding(4, 5, 4, 5);
+            tbMaPhong.Name = "tbMaPhong";
+            tbMaPhong.PasswordChar = '\0';
+            tbMaPhong.PlaceholderForeColor = Color.Gainsboro;
+            tbMaPhong.PlaceholderText = "";
+            tbMaPhong.SelectedText = "";
+            tbMaPhong.ShadowDecoration.CustomizableEdges = customizableEdges7;
+            tbMaPhong.Size = new Size(174, 39);
+            tbMaPhong.TabIndex = 8;
+            // 
+            // btDangXuat
+            // 
+            btDangXuat.Anchor = AnchorStyles.None;
+            btDangXuat.Animated = true;
+            btDangXuat.BackColor = Color.FromArgb(224, 224, 224);
+            btDangXuat.BorderRadius = 12;
+            btDangXuat.CustomizableEdges = customizableEdges8;
+            btDangXuat.DisabledState.BorderColor = Color.DarkGray;
+            btDangXuat.DisabledState.CustomBorderColor = Color.DarkGray;
+            btDangXuat.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btDangXuat.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btDangXuat.FillColor = Color.FromArgb(255, 107, 107);
+            btDangXuat.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btDangXuat.ForeColor = Color.Black;
+            btDangXuat.Location = new Point(50, 408);
+            btDangXuat.Margin = new Padding(4, 3, 4, 3);
+            btDangXuat.Name = "btDangXuat";
+            btDangXuat.ShadowDecoration.CustomizableEdges = customizableEdges9;
+            btDangXuat.Size = new Size(144, 39);
+            btDangXuat.TabIndex = 3;
+            btDangXuat.Text = "Đăng xuất";
+            // 
+            // btTaoPhong
+            // 
+            btTaoPhong.Anchor = AnchorStyles.None;
+            btTaoPhong.Animated = true;
+            btTaoPhong.BorderRadius = 12;
+            btTaoPhong.CustomizableEdges = customizableEdges10;
+            btTaoPhong.DisabledState.BorderColor = Color.DarkGray;
+            btTaoPhong.DisabledState.CustomBorderColor = Color.DarkGray;
+            btTaoPhong.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btTaoPhong.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btTaoPhong.FillColor = Color.FromArgb(160, 210, 235);
+            btTaoPhong.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btTaoPhong.ForeColor = Color.Black;
+            btTaoPhong.Location = new Point(378, 259);
+            btTaoPhong.Margin = new Padding(4, 3, 4, 3);
+            btTaoPhong.Name = "btTaoPhong";
+            btTaoPhong.ShadowDecoration.CustomizableEdges = customizableEdges11;
+            btTaoPhong.Size = new Size(174, 49);
+            btTaoPhong.TabIndex = 0;
+            btTaoPhong.Text = "Tạo phòng";
+            // 
+            // cpbLogo
+            // 
+            cpbLogo.Anchor = AnchorStyles.None;
+            cpbLogo.BackColor = Color.FromArgb(250, 250, 250);
+            cpbLogo.FillColor = Color.Gainsboro;
+            cpbLogo.ImageRotate = 0F;
+            cpbLogo.Location = new Point(378, 59);
+            cpbLogo.Margin = new Padding(4, 3, 4, 3);
+            cpbLogo.Name = "cpbLogo";
+            cpbLogo.ShadowDecoration.CustomizableEdges = customizableEdges12;
+            cpbLogo.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            cpbLogo.Size = new Size(174, 168);
+            cpbLogo.TabIndex = 9;
+            cpbLogo.TabStop = false;
+            // 
+            // cpbAvatar
+            // 
+            cpbAvatar.Anchor = AnchorStyles.None;
+            cpbAvatar.BackColor = Color.FromArgb(224, 224, 224);
+            cpbAvatar.FillColor = Color.LightGray;
+            cpbAvatar.ImageRotate = 0F;
+            cpbAvatar.Location = new Point(71, 189);
+            cpbAvatar.Margin = new Padding(4, 3, 4, 3);
+            cpbAvatar.Name = "cpbAvatar";
+            cpbAvatar.ShadowDecoration.CustomizableEdges = customizableEdges13;
+            cpbAvatar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            cpbAvatar.Size = new Size(102, 97);
+            cpbAvatar.TabIndex = 11;
+            cpbAvatar.TabStop = false;
+            // 
+            // panel_main
+            // 
+            panel_main.Controls.Add(lbPlayer);
+            panel_main.Controls.Add(lbDangChoi);
+            panel_main.Controls.Add(lbThamGia);
+            panel_main.Controls.Add(cpbAvatar);
+            panel_main.Controls.Add(cpbLogo);
+            panel_main.Controls.Add(btTaoPhong);
+            panel_main.Controls.Add(btDangXuat);
+            panel_main.Controls.Add(tbMaPhong);
+            panel_main.Controls.Add(btHoSo);
+            panel_main.Controls.Add(btThamGia);
+            panel_main.Controls.Add(shPlayer);
+            panel_main.Dock = DockStyle.Fill;
+            panel_main.Location = new Point(0, 0);
+            panel_main.Margin = new Padding(4, 3, 4, 3);
+            panel_main.Name = "panel_main";
+            panel_main.Size = new Size(921, 621);
+            panel_main.TabIndex = 12;
+            // 
+            // lbPlayer
+            // 
+            lbPlayer.Anchor = AnchorStyles.None;
+            lbPlayer.BackColor = Color.FromArgb(224, 224, 224);
+            lbPlayer.Font = new Font("Microsoft Sans Serif", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbPlayer.Location = new Point(96, 292);
+            lbPlayer.Margin = new Padding(4, 3, 4, 3);
+            lbPlayer.Name = "lbPlayer";
+            lbPlayer.Size = new Size(50, 22);
+            lbPlayer.TabIndex = 14;
+            lbPlayer.Text = "Player";
+            lbPlayer.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // lbDangChoi
+            // 
+            lbDangChoi.Anchor = AnchorStyles.None;
+            lbDangChoi.BackColor = Color.Transparent;
+            lbDangChoi.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbDangChoi.Location = new Point(363, 473);
+            lbDangChoi.Margin = new Padding(4, 3, 4, 3);
+            lbDangChoi.Name = "lbDangChoi";
+            lbDangChoi.Size = new Size(210, 20);
+            lbDangChoi.TabIndex = 13;
+            lbDangChoi.Text = "Hiện đang có 0 người đang chơi";
+            lbDangChoi.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // lbThamGia
+            // 
+            lbThamGia.Anchor = AnchorStyles.None;
+            lbThamGia.BackColor = Color.Transparent;
+            lbThamGia.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbThamGia.Location = new Point(413, 334);
+            lbThamGia.Margin = new Padding(4, 3, 4, 3);
+            lbThamGia.Name = "lbThamGia";
+            lbThamGia.Size = new Size(99, 20);
+            lbThamGia.TabIndex = 12;
+            lbThamGia.Text = "Hoặc tham gia";
+            lbThamGia.TextAlignment = ContentAlignment.MiddleCenter;
+            // 
+            // main_menu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(250, 250, 250);
+            ClientSize = new Size(921, 621);
+            Controls.Add(panel_main);
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "main_menu";
+            Text = "Main Menu";
+            ((System.ComponentModel.ISupportInitialize)cpbLogo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)cpbAvatar).EndInit();
+            panel_main.ResumeLayout(false);
+            panel_main.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Guna.UI2.WinForms.Guna2Shapes shPlayer;
+        private Guna.UI2.WinForms.Guna2Button btThamGia;
+        private Guna.UI2.WinForms.Guna2Button btHoSo;
+        private Guna.UI2.WinForms.Guna2TextBox tbMaPhong;
+        private Guna.UI2.WinForms.Guna2Button btDangXuat;
+        private Guna.UI2.WinForms.Guna2Button btTaoPhong;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox cpbLogo;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox cpbAvatar;
+        private System.Windows.Forms.Panel panel_main;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbThamGia;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbDangChoi;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lbPlayer;
+    }
+}
+
---
+++ main_menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FunDraw
+{
+    public partial class main_menu : Form
+    {
+        public main_menu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
