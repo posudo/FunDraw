2248c86af2b2c559b0cb80f63f9c09e9518709e0
tuan-te <taquoctuan01102005@gmail.com>
2024-12-01 23:44:43 +0700

    Fix: Delete feature forgot password success and add feature change password

---
+++ ChangePassword.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace FunDraw
+{
+    partial class ChangePassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            tbEmail = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2TextBox2 = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            btSend = new Guna.UI2.WinForms.Guna2Button();
+            SuspendLayout();
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(181, 195);
+            label2.Name = "label2";
+            label2.Size = new Size(163, 28);
+            label2.TabIndex = 3;
+            label2.Text = "Mật khẩu hiện tại";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(181, 254);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Mật khẩu mới";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(181, 316);
+            label3.Name = "label3";
+            label3.Size = new Size(177, 28);
+            label3.TabIndex = 5;
+            label3.Text = "Xác nhận mật khẩu";
+            // 
+            // tbEmail
+            // 
+            tbEmail.Anchor = AnchorStyles.None;
+            tbEmail.BorderRadius = 12;
+            tbEmail.Cursor = Cursors.IBeam;
+            tbEmail.CustomizableEdges = customizableEdges1;
+            tbEmail.DefaultText = "";
+            tbEmail.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            tbEmail.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            tbEmail.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            tbEmail.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            tbEmail.FillColor = Color.FromArgb(224, 224, 224);
+            tbEmail.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbEmail.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbEmail.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            tbEmail.Location = new Point(422, 195);
+            tbEmail.Margin = new Padding(5, 7, 5, 7);
+            tbEmail.Name = "tbEmail";
+            tbEmail.PasswordChar = '\0';
+            tbEmail.PlaceholderForeColor = Color.Gainsboro;
+            tbEmail.PlaceholderText = "";
+            tbEmail.SelectedText = "";
+            tbEmail.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            tbEmail.Size = new Size(292, 28);
+            tbEmail.TabIndex = 20;
+            // 
+            // guna2TextBox1
+            // 
+            guna2TextBox1.Anchor = AnchorStyles.None;
+            guna2TextBox1.BorderRadius = 12;
+            guna2TextBox1.Cursor = Cursors.IBeam;
+            guna2TextBox1.CustomizableEdges = customizableEdges3;
+            guna2TextBox1.DefaultText = "";
+            guna2TextBox1.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox1.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox1.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox1.FillColor = Color.FromArgb(224, 224, 224);
+            guna2TextBox1.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2TextBox1.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox1.Location = new Point(422, 254);
+            guna2TextBox1.Margin = new Padding(5, 7, 5, 7);
+            guna2TextBox1.Name = "guna2TextBox1";
+            guna2TextBox1.PasswordChar = '\0';
+            guna2TextBox1.PlaceholderForeColor = Color.Gainsboro;
+            guna2TextBox1.PlaceholderText = "";
+            guna2TextBox1.SelectedText = "";
+            guna2TextBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2TextBox1.Size = new Size(292, 28);
+            guna2TextBox1.TabIndex = 21;
+            // 
+            // guna2TextBox2
+            // 
+            guna2TextBox2.Anchor = AnchorStyles.None;
+            guna2TextBox2.BorderRadius = 12;
+            guna2TextBox2.Cursor = Cursors.IBeam;
+            guna2TextBox2.CustomizableEdges = customizableEdges5;
+            guna2TextBox2.DefaultText = "";
+            guna2TextBox2.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            guna2TextBox2.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            guna2TextBox2.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            guna2TextBox2.FillColor = Color.FromArgb(224, 224, 224);
+            guna2TextBox2.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            guna2TextBox2.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            guna2TextBox2.Location = new Point(422, 316);
+            guna2TextBox2.Margin = new Padding(5, 7, 5, 7);
+            guna2TextBox2.Name = "guna2TextBox2";
+            guna2TextBox2.PasswordChar = '\0';
+            guna2TextBox2.PlaceholderForeColor = Color.Gainsboro;
+            guna2TextBox2.PlaceholderText = "";
+            guna2TextBox2.SelectedText = "";
+            guna2TextBox2.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2TextBox2.Size = new Size(292, 28);
+            guna2TextBox2.TabIndex = 22;
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.Anchor = AnchorStyles.None;
+            guna2HtmlLabel2.AutoSize = false;
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Font = new Font("Microsoft Sans Serif", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel2.ForeColor = SystemColors.WindowText;
+            guna2HtmlLabel2.Location = new Point(295, 89);
+            guna2HtmlLabel2.Margin = new Padding(5, 4, 5, 4);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(289, 59);
+            guna2HtmlLabel2.TabIndex = 23;
+            guna2HtmlLabel2.Text = "Đổi mật khẩu";
+            guna2HtmlLabel2.TextAlignment = ContentAlignment.TopCenter;
+            // 
+            // btSend
+            // 
+            btSend.Anchor = AnchorStyles.None;
+            btSend.Animated = true;
+            btSend.BorderRadius = 12;
+            btSend.CustomizableEdges = customizableEdges7;
+            btSend.DisabledState.BorderColor = Color.DarkGray;
+            btSend.DisabledState.CustomBorderColor = Color.DarkGray;
+            btSend.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btSend.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btSend.FillColor = Color.FromArgb(160, 210, 235);
+            btSend.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btSend.ForeColor = Color.Black;
+            btSend.Location = new Point(329, 415);
+            btSend.Margin = new Padding(5, 4, 5, 4);
+            btSend.Name = "btSend";
+            btSend.ShadowDecoration.CustomizableEdges = customizableEdges8;
+            btSend.Size = new Size(190, 53);
+            btSend.TabIndex = 24;
+            btSend.Text = "OK";
+            // 
+            // ChangePassword
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(875, 547);
+            Controls.Add(btSend);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(guna2TextBox2);
+            Controls.Add(guna2TextBox1);
+            Controls.Add(tbEmail);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Name = "ChangePassword";
+            Text = "ChangePassword";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Guna.UI2.WinForms.Guna2TextBox tbEmail;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2Button btSend;
+    }
+}
